57730f2a40f9d2433f33316f78058946e0de8611
Felipa Mendez <felipalouise@gmail.com>
2021-12-15 13:13:08 -0600

    completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+let number = 10
+function add(){
+    return (number += 5);
+}
+function subtract() {
+    return (number -= 5);
+}
+function multiply() {
+    return (number *= 5);
+}
+function divide() {
+    return (number /= 5);
+}
+
+let num1 = 1;
+let num2 = 2;
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiply(num1, num2) {
+ return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+
+const n = 1;
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
